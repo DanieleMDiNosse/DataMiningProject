c2ef56f47511dd8477667407b0eefee2d7679ed3
AngeloLasala <angelo.lasala96@gmail.com>
2020-11-03 20:07:45 +0100

    import figure and modified code

--- project.py
+++ project.py
@@ -4,15 +4,20 @@ Created on Tue Nov  3 10:29:52 2020
 
 @author: Daniele
 """
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('Test_HR_Employee_Attrition.csv')
 print(df.iloc[0], df.dtypes)
 
-# df.hist(column=14)
-plt.hist(df.values[:,14])
+plt.figure()
+plt.hist(df.values[:,3],bins=13)
+plt.xticks(np.arange(111,1594,106))
+plt.xlabel('Daily rate')
+plt.ylabel('Number of employed')
+plt.show()
+
 
 
 
