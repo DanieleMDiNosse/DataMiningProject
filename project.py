f0208f346ebaa890637f72fe058adf178df8efc4
RaffaeleParadiso <raffaele05@gmail.com>
2020-11-04 18:06:46 +0100

    added figures

--- project.py
+++ project.py
@@ -2,20 +2,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import math
+
 df = pd.read_csv('Test_HR_Employee_Attrition.csv')
 print(df.iloc[0], df.dtypes)
 
 # df.hist(column=14)
-plt.hist(df.values[:,14])
+# plt.hist(df.values[:,5])
 
 plt.figure()
-plt.hist(df.values[:,3],bins=13)
-plt.xticks(np.arange(111,1594,106))
-plt.xlabel('Daily rate')
-plt.ylabel('Number of employed')
+plt.hist(df.values[:,32],bins = 5, edgecolor='k')
+plt.xticks(np.arange(0,20.4, 3.4))
+plt.xlabel('Years With Curr Manager ')
+plt.ylabel('Number of employes')
+
+
+
+
+#bins = np.arange(1, 4 + 1.5) - 0.5, edgecolor='k'
+# print(df.dtypes)
+# print(df.iloc[0])
+
+# figure, axes = plt.subplots(4, 4)
+# # df['Age'].plot(ax=axes[0])
+# for i in range(0,4):
+#     for j in range(0,4):
+#         df['Age'].plot(ax=axes[i,j])
+# plt.show()
+
+
 plt.show()
-
-
-
-
-
