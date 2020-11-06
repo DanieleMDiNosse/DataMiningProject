1f10ea15ac5b8b96b820def3cbdf2ae05c14c92c
AngeloLasala <angelo.lasala96@gmail.com>
2020-11-06 16:32:30 +0100

    'i'

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 import math
 
@@ -11,19 +12,23 @@ print(df.iloc[0], df.dtypes)
 # plt.hist(df.values[:,5])
 
 plt.figure()
-plt.hist(df.values[:,32],bins = 5, edgecolor='k') #edge color per gli hist separati da una linea NEGRA
-plt.xticks(np.arange(0,20.4, 3.4))
-plt.xlabel('Years With Curr Manager ')
+plt.hist(df.values[:,24],bins = np.arange(1, 80 + 1.5) - 0.5, edgecolor='k') #edge color per gli hist separati da una linea NEGRA
+plt.xticks(np.arange(60,120,10))
+plt.xlabel('Standard Hours')
 plt.ylabel('Number of employes')
 
+# handles=[Line2D([0],[0],marker='o',color='w',linestyle='', markerfacecolor='w'),Line2D([0],[0],marker='o',color='w', markerfacecolor='w'),Line2D([0],[0],marker='o',color='w', markerfacecolor='w'),Line2D([0],[0],marker='o',color='w', markerfacecolor='w')]
+# labels=['1 Low','2 Medium','3 High','4 Very high']
+# plt.legend(handles,labels, edgecolor='w', loc='upper left')
 
 
-# l = [0,3,5,6,8,10,11,12,14,16,17,18,21,22,23,24,25,26,27,28,29,30,31,32]
-#for i in range(len(l)):
-#    print(f' {l[i]} : min e max di {df.columns[l[i]]} sono {min(df.values[:,l[i]])} e {max(df.values[:,l[i]])}. Range --> {max(df.values[:,l[i]])-min(df.values[:,l[i]])}.')
 
+l = [0,3,5,6,8,10,11,12,14,16,17,18,21,22,23,24,25,26,27,28,29,30,31,32]
+for i in range(len(l)):
+   print(f' {l[i]} : min e max di {df.columns[l[i]]} sono {min(df.values[:,l[i]])} e {max(df.values[:,l[i]])}. Range --> {max(df.values[:,l[i]])-min(df.values[:,l[i]])}.')
 
-#bins = np.arange(1, 4 + 1.5) - 0.5, edgecolor='k' ###per centrare i bin
+print(len(l))
+
 # print(df.dtypes)
 # print(df.iloc[0])
 
