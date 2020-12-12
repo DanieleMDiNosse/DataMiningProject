3d4421dc660b8c353177224190d1188243a1b5d5
AngeloLasala <angelo.lasala96@gmail.com>
2020-12-12 17:26:36 +0100

    add validation hierarchical part

--- Codici/validation_random.py
+++ Codici/validation_random.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 from scipy.stats import mode
 from scipy.spatial.distance import pdist
-from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 #== PARAMETERS ===============================================================================
 num_records=899         #numer of records for each attributes (see random_attributes)
-validation_times=500    #numer of random data frame (see ciclo for)
+validation_times=20    #numer of random data frame (see ciclo for)
 #============================================================================================
 
 #== IF CHECK =================================================================================
@@ -73,7 +73,7 @@ if kmean_validation:
         kmeans=KMeans(init='k-means++', n_clusters=4, n_init=100, max_iter=300)
         kmeans.fit(X)
         sse=kmeans.inertia_
-        print(time, sse)
+        print(f'{time+1}) SSE: {sse}')
         ist_sse.append(sse)
 
     ist_sse=np.array(ist_sse)
@@ -97,18 +97,44 @@ if kmean_validation:
 # == HIERARCHICAL ================================================================
 
 if hierarchical_validation:
-    PercentSalaryHike = random_attributes('PercentSalaryHike', 3.316, 5.0)
-    FractionYearsAtCompany = random_attributes('FractionYearsAtCompany',0.0,1.0)
-    TrainingTimesLastYear = random_attributes('TrainingTimesLastYear', 0.0, 6.0)
-    RateIncome = random_attributes('RateIncome', 0.044, 0.997)
-    NumCompaniesWorked = random_attributes('NumCompaniesWorked', 0.0, 3.0)
+    silhouette_list=list()
+    for time in range(validation_times):
+        PercentSalaryHike = random_attributes('PercentSalaryHike', 3.316, 5.0)
+        FractionYearsAtCompany = random_attributes('FractionYearsAtCompany',0.0,1.0)
+        TrainingTimesLastYear = random_attributes('TrainingTimesLastYear', 0.0, 6.0)
+        RateIncome = random_attributes('RateIncome', 0.044, 0.997)
+        NumCompaniesWorked = random_attributes('NumCompaniesWorked', 0.0, 3.0)
+        Age = random_attributes('Age', 18, 60, integer=True)
+        StockOptionLevel = random_attributes('StockOptionLevel', 0, 3 , integer=True)
+        MonthlyRate = random_attributes('MonthlyRate', 45.9, 164.4)
+        MonthlyIncome = random_attributes('MonthlyIncome', 6.915, 9.905)
 
-    df = random_dataframe([PercentSalaryHike,FractionYearsAtCompany,TrainingTimesLastYear,RateIncome,NumCompaniesWorked])
+        df = random_dataframe([TrainingTimesLastYear,Age,StockOptionLevel])
 
-    scaler = MinMaxScaler()
-    X = scaler.fit_transform(df)
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(df)
 
-    data_dist = pdist(X, metric='euclidean')
-    data_link = linkage(data_dist, method='average', metric='euclidean', optimal_ordering = True)
-    res = dendrogram(data_link, color_threshold=7.8)   
+        n_clu=3
+        data_dist = pdist(X, metric='euclidean')
+        data_link = linkage(data_dist, method='ward', metric='euclidean', optimal_ordering = True)
+        fclu = fcluster(data_link, n_clu, criterion="maxclust")
+        # res = dendrogram(data_link, color_threshold=7.8)   
+        sil = silhouette_score(X, fclu, metric='euclidean')
+        print(f'{time+1}) silhouette: {sil}')
+        silhouette_list.append(sil)
+    
+    silhouette_list=np.array(silhouette_list)
+    with open("validation_hierarchical_1.txt",'w', encoding='utf-8') as f:
+        f.write(f'DATA FRAME\n')
+        f.write(f'{df.columns}\n')
+        f.write(f'\n')
+        f.write(f'number of values for each attributes: {num_records}\n')
+        f.write(f'number of random data frames: {validation_times}\n')
+        f.write(f'number of clusters: {n_clu}\n')
+        f.write(f'_____________________________________________________________\n')
+        f.write(f'\n')
+        f.write(f'STATISTIC\n')
+        f.write(f'mean (silhouette): {silhouette_list.mean()}\n')
+        f.write(f'std (silouette): {silhouette_list.std()}\n')
+
     plt.show() 
